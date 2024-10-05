d3af159683c1fee951478a0ce24590c6f726f7e5
Aalekh-Coder <dheetaalekh@gmai.com>
2024-10-05 08:47:56 +0530

    day 2

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -19,8 +19,8 @@ const RecentProjects = () => {
             key={item.id}
           >
             <PinContainer
-              title="/ui.aceternity.com"
-              href="https://twitter.com/mannupaaji"
+              title="github.com/Aalekh-coder"
+              href="https://github.com/Aalekh-coder/FullStack-Project-1"
             >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
@@ -32,7 +32,7 @@ const RecentProjects = () => {
                 <img
                   src={item.img}
                   alt="cover"
-                  className="z-10 absolute bottom-0"
+                  className="z-10 absolute bottom-0 top-0"
                 />
               </div>
 
--- data/index.ts
+++ data/index.ts
@@ -74,11 +74,11 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "https://github.com/adrianhajdin?tab=repositories",
+    title: "Wanderlust: The Ultimate Hotel Management System for Travelers ",
+    des: "Wanderlust could be the ideal solution. It not only caters to hotels but also enhances the travel experience by integrating essential features",
+    img: "/wanerlust3.jpg",
+    iconLists: ["/bootstrap-4.svg", "/ejs.svg", "/js.svg", "/expressjs.svg", "/mongodb.svg"],
+    link: "https://github.com/Aalekh-coder/FullStack-Project-1",
   },
   {
     id: 2,
